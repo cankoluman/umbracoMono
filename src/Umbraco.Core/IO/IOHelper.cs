13e3cdfd50c1055bb41fee37b9e57735af08f7a6
m57j75 <n2dbrx@gmail.com>
2013-02-24 16:46:19 +0000

    merge in multiplatrom io to correct ~ in paths issue

--- src/Umbraco.Core/IO/IOHelper.cs
+++ src/Umbraco.Core/IO/IOHelper.cs
@@ -82,7 +82,7 @@ namespace Umbraco.Core.IO
             return text;
         }
 
-        public static string MapPath(string path, bool useHttpContext)
+        public static string MapPathBase(string path, bool useHttpContext)
         {
             // Check if the path is already mapped
             if ((path.Length >= 2 && path[1] == Path.VolumeSeparatorChar) || path.StartsWith("\\"))
@@ -109,6 +109,15 @@ namespace Umbraco.Core.IO
         	var retval = root + IOHelper.DirSepChar.ToString() + newPath;
 
         	return retval;
+        }
+
+        public static string MapPath(string path, bool useHttpContext)
+        {
+
+			if (IO.MultiPlatformHelper.IsWindows)
+				return MapPathBase(path, useHttpContext);            
+
+			return IO.MultiPlatformHelper.MapUnixPath(path, useHttpContext);
         }
 
         public static string MapPath(string path)
---
+++ src/Umbraco.Core/IO/MultiPlatformHelper.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Text;
+
+namespace Umbraco.Core.IO
+{
+	public static class MultiPlatformHelper
+	{
+
+		public const string PLATFORM_UNIX = "UNIX";
+		public const string PLATFORM_WIN = "WIN";
+
+		public const string WIN_DIRSEP = "\\";
+		public const string UNIX_DIRSEP = "/";
+
+		public static string OSPlatform
+		{
+			get
+			{
+				return System.Environment.OSVersion.Platform.ToString().ToUpper();
+			}
+		}
+
+		public static bool IsWindows
+		{
+			get
+			{
+				return OSPlatform.Contains(PLATFORM_WIN);
+			}
+		}
+
+		public static bool IsUnix
+		{
+			get
+			{
+				return OSPlatform.Contains(PLATFORM_UNIX);
+			}
+		}
+
+		public static string MapUnixPath(string path)
+		{
+			string filePath = path;
+
+			if (filePath.StartsWith("~"))
+				filePath = IOHelper.ResolveUrl(filePath);
+
+			filePath = IOHelper.MapPathBase(filePath, System.Web.HttpContext.Current != null);
+
+			return filePath;
+		}
+
+		public static string MapUnixPath(string path, bool useHttpContext)
+		{
+			string filePath = path;
+
+			if (filePath.StartsWith("~"))
+				filePath = IOHelper.ResolveUrl(filePath);
+
+			filePath = IOHelper.MapPathBase(filePath, useHttpContext);
+
+			return filePath;
+		}
+
+		public static string ConvertPathFromUnixToWin(string path)
+		{
+			return path.Replace(MultiPlatformHelper.UNIX_DIRSEP, MultiPlatformHelper.WIN_DIRSEP);
+		}
+
+		public static string EnsureRootAppPath (string path)
+		{
+			if (IsUnix && (path == String.Empty))
+				return "/";
+			return path;
+		}
+
+	}
+}
+
